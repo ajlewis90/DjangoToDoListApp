961b977002d35acc1773e3bb6461c28287febb02
ajlewis90 <arthur.lewis420@gmail.com>
2025-08-11 09:46:16 +1200

    Updated requirements.txt and ran migrations

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todoapp_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Cellfone01',#'This is the password for your local postgres pgadmin'
+        'HOST': '', #'Localhost is empty'
+        'PORT':'', #Assumes default as 5432
+    },
 }
 
 
